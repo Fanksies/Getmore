f470cdbe03288ff7f2b43aa268c5eea8c8c1e901
Arturo <Ruiz>
2019-05-01 18:44:08 -0500

    add: styles to hero section

--- pages/index.vue
+++ pages/index.vue
@@ -7,27 +7,30 @@
           <div>ball</div>
         </div>
       </div>
-      <nav class="container">
+
+      <div class="title container">
       
         <b-navbar>
           <b-navbar-nav>
-            <b-nav-item class="active" href="#"> Inicio</b-nav-item>
+            <b-nav-item class="active" href="#"> <strong>Inicio</strong></b-nav-item>
             <b-nav-item href="#">Acerca de nosotros</b-nav-item>
             <b-nav-item href="#">Historia</b-nav-item>
             <b-nav-item href="#">Contacto</b-nav-item>
           </b-navbar-nav>
         </b-navbar>  
 
-        <ul class="ml-auto">
+        <!-- <ul class="ml-auto">
           <li>Es</li>
           <li>En</li>          
-        </ul>
+        </ul> -->
 
-      </nav>
-      <div class="title">
+      <div class="flex-2 pl-3">
         <h1>Las mejores raquetas para <br> jugar <span class="title__highlight">Tennis</span> </h1>
         <button>Acerca de Nosotros</button>
       </div>
+
+      </div>
+      
     </section>
 
     <section id="history">
@@ -111,23 +114,68 @@ export default {
 
 <style lang="scss">
 
+// Variables
+
+$green: #cbe346;
+
 // Generals
 
 html, body {
   font-family: Arial, Helvetica, sans-serif;
 }
 
+button {
+  all: unset;
+}
+
+.flex-2 {
+  flex:2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
 // Sections
 
-  //Navbar 
-    .nav-item.active {
-      font-weight:bold;
-    }
+// Hero 
 
-    .navbar-light .navbar-nav .nav-link {
-      letter-spacing: 1.5px;
-      color:white;
-    }
+#hero {
+  display: grid;
+  grid-template-columns: 100px 1fr;
+  h1 {
+    font-size:3.31em;
+    line-height: 1.28;
+    color:white;
+  }
+  .title {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+}
+
+.aside {
+  border-right: 2px solid white;
+}
+
+.title__highlight {
+  color: $green !important;
+}
+
+//Navbar 
+.nav-link  {
+    color:white !important;
+    letter-spacing: 1.5px;
+}
+
+.nav-item {
+  margin:0 1.5em;  
+}
+
+.nav-item.active {
+  margin-left:0 !important;
+}
 
 #hero {
   background-image: url('/group.png');
